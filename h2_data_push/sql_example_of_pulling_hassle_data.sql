a49f71da17ba56621b95a13c094084fcd32dea44
miroahti <miro.ahti@helpling.com>
2015-12-11 15:53:06 +0100

    hassle data

--- h2_data_push/sql_example_of_pulling_hassle_data.sql
+++ h2_data_push/sql_example_of_pulling_hassle_data.sql
@@ -3,73 +3,54 @@
 -- LOCATIONS:
 
 SELECT 
-    pa.user_id,
-    pc.name
+    *
 FROM 
     provider_areas pa
     JOIN postcodes pc on pa.postcode_id = pc.id
 WHERE
-    pc.country = 'France'
+    pc.country = 'France' ;
 
     
 -- AVAILABILITIES    
 
 -- Time, when not available.
 
--- busy times 
 SELECT
-	user_id,
-	starttime,
-	endtime,
-	'busy_time'::text as reason
+	bt.*
 FROM 
 	busy_times bt
 JOIN 
 	users u on u.id = bt.id
 WHERE 
-	u.country = 'France'
-	AND starttime >= current_date
+	u.country = 'France' ;
 
-UNION ALL
 
 SELECT
-    user_id,
-    start as starttime,
-    finish as endtime,
-    'holiday' as reason
+    h.*
 FROM 
     holidays h
 JOIN 
 	users u on u.id = h.id
 WHERE 
-	u.country = 'France'
-	AND start >= current_date
+	u.country = 'France' ;
 
-UNION ALL 
+
 
 SELECT 
-    provider_user_id AS user_id,
-    starttime,
-    endtime,
-    'event' as reason
+    e.*
 FROM 
     events e 
 JOIN 
 	users u on e.provider_user_id = u.id
 WHERE 
-    NOT cancelled
-    AND u.country = 'France'
-	AND starttime >= current_date
+    u.country = 'France' ;
 
 
 
 -- FINALLY THE AVAILABLE TIMES OF THE PROVIDERS
 
-select
-user_id,
-day,
-open_time,
-close_time
-from working_hours
-where available
-limit 1000
+SELECT
+    *
+FROM 
+    working_hours ;
+
